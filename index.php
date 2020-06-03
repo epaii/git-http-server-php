af75efdfba079adf074448da557d39759f3f4661
MrRen <543169072@qq.com>
2020-06-03 11:17:57 +0800

    init

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 error_reporting(0);
 require_once __DIR__ . "/funs.php";
 $config = json_decode(file_get_contents(__DIR__ . "/config.json"), true);
-// if (!isset($_SERVER['PHP_AUTH_USER'])) {
-//     header('WWW-Authenticate: Basic realm="epii server git"');
-//     header('HTTP/1.0 401 Unauthorized');
-//     echo 'shibai';
-//     exit;
-// } else {
-//     if (!($_SERVER['PHP_AUTH_USER'] == $config["username"] && $_SERVER['PHP_AUTH_PW'] == $config["password"])) {
-//         header('WWW-Authenticate: Basic realm="epii server git"');
-//         header('HTTP/1.0 401 Unauthorized');
-//         echo 'shibai';
-//         exit;
-//     }
-// }
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="epii server git"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'shibai';
+    exit;
+} else {
+    if (!($_SERVER['PHP_AUTH_USER'] == $config["username"] && $_SERVER['PHP_AUTH_PW'] == $config["password"])) {
+        header('WWW-Authenticate: Basic realm="epii server git"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'shibai';
+        exit;
+    }
+}
 server_start($config);
