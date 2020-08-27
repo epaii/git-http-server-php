fc4193374755c6c5eb9d8baa3bcf888cf4ff21bf
MrRen <543169072@qq.com>
2020-08-27 11:19:52 +0800

    init

--- funs.php
+++ funs.php
@@ -1,11 +1,10 @@
 <?php
 
-
 // uncomporess content
 function gzBody($gzData)
 {
 
-    $encoding = isset($_SERVER['HTTP_CONTENT_ENCODING'])?$_SERVER['HTTP_CONTENT_ENCODING']:"";
+    $encoding = isset($_SERVER['HTTP_CONTENT_ENCODING']) ? $_SERVER['HTTP_CONTENT_ENCODING'] : "";
     $gzip = ($encoding == 'gzip' || $encoding == 'x-gzip');
     if (!$gzip) {
         return $gzData;
@@ -17,11 +16,55 @@ function gzBody($gzData)
             list($xlen) = unpack('v', substr($gzData, $i, 2));
             $i = $i + 2 + $xlen;
         }
-        if ($flg & 8) $i = strpos($gzData, "\0", $i) + 1;
-        if ($flg & 16) $i = strpos($gzData, "\0", $i) + 1;
-        if ($flg & 2) $i = $i + 2;
+        if ($flg & 8) {
+            $i = strpos($gzData, "\0", $i) + 1;
+        }
+
+        if ($flg & 16) {
+            $i = strpos($gzData, "\0", $i) + 1;
+        }
+
+        if ($flg & 2) {
+            $i = $i + 2;
+        }
+
     }
     return gzinflate(substr($gzData, $i, -8));
+}
+
+function check($name, $config)
+{
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        header('WWW-Authenticate: Basic realm="epii server git"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'no auth';
+        exit;
+    } else {
+        $ok = true;
+        if (isset($config["username"]) && $config["username"]) {
+            $ok = ($_SERVER['PHP_AUTH_USER'] == $config["username"]) && ($_SERVER['PHP_AUTH_PW'] == $config["password"]);
+        } else if (isset($config["auth"])) {
+            if (isset($config["auth"]["username"]) && $config["auth"]["username"]) {
+                $ok = ($_SERVER['PHP_AUTH_USER'] == $config["auth"]["username"]) && ($_SERVER['PHP_AUTH_PW'] == $config["auth"]["password"]);
+            } else if (!isset($config["auth"]['auth'][$name])) {
+                $ok = false;
+            } else {
+                if (!in_array($_SERVER['PHP_AUTH_USER'], $config["auth"]['auth'][$name])) {
+                    $ok = false;
+                } else {
+                    $ok = $_SERVER['PHP_AUTH_PW'] == $config["auth"]["user"][$_SERVER['PHP_AUTH_USER']];
+                }
+
+            }
+        }
+
+        if (!$ok) {
+            header('WWW-Authenticate: Basic realm="epii server git"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo 'shibai';
+            exit;
+        }
+    }
 }
 
 function server_start($config)
@@ -29,29 +72,30 @@ function server_start($config)
 
     $writeLog = function ($obj) use ($config) {
 
-        if(isset($config["debug"]) && $config["debug"])
-        {
+        if (isset($config["debug"]) && $config["debug"]) {
             if (is_scalar($obj)) {
                 $msg = $obj;
             } else {
-                $msg = var_export($obj,true);
+                $msg = var_export($obj, true);
             }
-            file_put_contents($config["log_dir"]."/".date("Ymd").".log", $msg . PHP_EOL, FILE_APPEND);
+            file_put_contents($config["log_dir"] . "/" . date("Ymd") . ".log", $msg . PHP_EOL, FILE_APPEND);
         }
-      
+
     };
     $git_command = $config["git_command"];
     $uri = $_GET['git'];
-    $name = explode(".git",$uri)[0].".git";
-    $path =  $config["repos_dir"]."/".$name;
-    $action = str_replace($name."/","",$uri);
-    $writeLog("action:".$action);
+    $name = explode(".git", $uri)[0] . ".git";
+    check($name, $config);
+    $path = $config["repos_dir"] . "/" . $name;
+
+    $action = str_replace($name . "/", "", $uri);
+    $writeLog("action:" . $action);
     switch ($action) {
         case 'info/refs':
             $service = $_GET['service'];
             $writeLog('service:' . $service);
-             header('Content-type: application/x-' . $service . '-advertisement');
-            $cmd = sprintf($git_command.' %s --stateless-rpc --advertise-refs %s', substr($service, 4), $path);
+            header('Content-type: application/x-' . $service . '-advertisement');
+            $cmd = sprintf($git_command . ' %s --stateless-rpc --advertise-refs %s', substr($service, 4), $path);
             $writeLog('cmd:' . $cmd);
             exec($cmd, $outputs);
             //print_r($outputs);
@@ -59,7 +103,7 @@ function server_start($config)
             $length = strlen($serverAdvert) + 4;
 
             echo sprintf('%04x%s0000', $length, $serverAdvert);
-            $content =  implode("\n", $outputs);
+            $content = implode("\n", $outputs);
             //$content= str_replace(base64_decode("AA==")," ",$content);
             echo $content;
             $writeLog('content:' . $content);
@@ -73,7 +117,7 @@ function server_start($config)
             header(sprintf('Content-type: application/x-%s-result', $action));
             $input = gzBody($input);
             // writeLog("input:".$input);
-            $cmd = sprintf($git_command.' %s --stateless-rpc %s', substr($action, 4), $path);
+            $cmd = sprintf($git_command . ' %s --stateless-rpc %s', substr($action, 4), $path);
             $descs = [
                 0 => ['pipe', 'r'],
                 1 => ['pipe', 'w'],
